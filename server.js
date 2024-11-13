dd5981992c804cef7f556c491218ab47f8ca9555
Daniel Rivas <daniel.rivas.guzman@gmail.com>
2024-11-13 15:47:50 -0300

    Inicializar proyecto de juego de preguntas

--- server.js
+++ server.js
@@ -173,7 +173,10 @@ function endGame() {
     Object.values(players).forEach(player => player.answered = false);
 }
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
